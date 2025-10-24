8dc2337bb343286d9df10af7d94c69a60cfed7ae
ELMINYAWE <elmnyawe65@gmail.com>
2025-10-24 22:57:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/ELMINYAWE-Web/',
+      base: '/ELMINYAWE/',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -21,7 +21,7 @@ export default defineConfig(({ mode }) => {
         }
       },
       css: {
-        postcss: './postcss.config.js',
+        postcss: './postcss.config.cjs',
       }
     };
 });
